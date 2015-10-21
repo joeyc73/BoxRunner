b826e78b84b01e979930e7969fba355aa9570f13
joeyc73 <jrcarlos@ucsc.edu>
2015-10-21 11:57:10 -0700

    Game Files

---
+++ WebSite1/gameFile.js
@@ -0,0 +1,204 @@
+﻿(function () {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var canvas = document.getElementById("canvas"),
+ctx = canvas.getContext("2d"),
+width = 900,
+height = 200,
+level = 1,
+player = {
+    x: width / 2,
+    y: height - 15,
+    width: 15,
+    height: 15,
+    speed: 3,
+    velX: 0,
+    velY: 0,
+    jumping: false,
+    grounded: false
+},
+keys = [];
+canvas.width = width;
+canvas.height = height;
+var boxes = initLevel();
+specials = [];
+var bounce = new Audio("bounce.wav");
+var goal = new Image();
+goal.src = "goal.png";
+var redButtonUp = new Image();
+redButtonUp.src = "redButtonUP.png";
+var redButtonDown = new Image();
+redButtonDown.src = "redButtonDOWN.png";
+var platformColor = "white";
+var platformLevelColor = "white";
+
+function update() {
+
+    // check keys
+    if (keys[38]) {
+        // up arrow
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            player.velY = -player.speed * 1.5;
+            bounce.currentTime = 0;
+            bounce.play();
+        }
+    }
+    if (keys[39]) {
+        // right arrow
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
+    }
+    if (keys[37]) {
+        // left arrow                  
+        if (player.velX > -player.speed) {
+            player.velX--;
+        }
+    }
+
+    friction = .8;
+    gravity = 0.3;
+    if (!player.grounded) {
+        friction = .9
+        player.velY += gravity;
+        player.y += player.velY;
+    }
+    player.velX *= friction;
+    //Prepare to draw
+    ctx.clearRect(0, 0, width, height);
+    //Draw Platforms
+    ctx.fillStyle = platformColor;
+    ctx.beginPath();
+    drawBoxes();
+    platformColor = platformLevelColor;
+    if (player.grounded) {
+        player.velY = 0;
+    }
+
+    player.x += player.velX;
+    player.y += player.velY;
+    ctx.fill();
+
+    drawSpecials();
+
+    //Draw Goal
+    ctx.drawImage(goal, boxes[boxes.length - 1].x, boxes[boxes.length - 1].y);
+
+    
+
+    if (colCheck(player, boxes[boxes.length - 1])) {
+        initLevel(++level);
+    }
+    //Draw Player
+    ctx.fillStyle = "#2EFEC8";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    // run through the loop again
+    requestAnimationFrame(update);
+}
+
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null;
+
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        // figures out on which side we are colliding (top, bottom, left, or right)         
+        var oX = hWidths - Math.abs(vX),
+			oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
+function initLevel(){
+    boxes = [];
+    specials = [];
+    player.x = 300;
+    player.y = height - 20;
+    boxes = loadLevel(level);
+    platformColor = platformLevelColor;
+    document.getElementById("level").innerHTML = "Level : " + level.toString();
+    return boxes;
+}
+
+function drawBoxes() {
+    player.grounded = false;
+    for (var i = 0; i < boxes.length - 1; i++) {
+        ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+        var dir = colCheck(player, boxes[i]);
+        if (dir == "l" || dir == "r") {
+            player.velX = 0;
+            player.jumping = false;
+        }
+        else if (dir == "b") {
+            player.grounded = true;
+            player.jumping = false;
+        }
+        else if (dir == "t") {
+            player.velY *= -1;
+        }
+    }
+}
+
+function drawSpecials() {
+    //Draw Special Items
+    for (var i = 0; i < specials.length; i++) {
+        var dir = colCheck(player, specials[i]);
+        if (dir == "l" || dir == "r") {
+            player.velX = 0;
+            player.jumping = false;
+        }
+        else if (dir == "b") {
+            player.grounded = true;
+            player.jumping = false;
+            if (specials[i].type == "lightButton") {
+                ctx.drawImage(redButtonDown, specials[i].x, specials[i].y-5);
+                platformColor = "white";
+            }
+        }
+        else if (dir == "t") {
+            player.velY *= -1;
+        }
+        if (specials[i].type == "lightButton" && dir != "b") {
+            ctx.drawImage(redButtonUp, specials[i].x, specials[i].y-5);
+        }
+    }
+}
+
+window.addEventListener("load", function () {
+    update();
+});
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
---
+++ WebSite1/gameLevels.js
@@ -0,0 +1,535 @@
+﻿function loadLevel(level) {
+    var boxes = []
+    boxes.push({
+        x: 0,
+        y: height,
+        width: width,
+        height: 1
+    })
+    boxes.push({
+        x: 0,
+        y: 0,
+        width: 1,
+        height: height
+    })
+    boxes.push({
+        x: width,
+        y: 0,
+        width: 1,
+        height: height
+    })
+    boxes.push({
+        x: 0,
+        y: 0,
+        width: width,
+        height: 1
+    })
+
+    if (level == 1) {
+        boxes.push({
+            x: 30,
+            y: height - 30,
+            width: 30,
+            height: 10
+        });
+        boxes.push({
+            x: 800,
+            y: height - 30,
+            width: 30,
+            height: 10
+        });
+        boxes.push({
+            x: 600,
+            y: height - 40,
+            width: 50,
+            height: 10
+        })
+        boxes.push({
+            x: 80,
+            y: height - 60,
+            width: 300,
+            height: 10
+        })
+        boxes.push({
+            x: 400,
+            y: height - 100,
+            width: 100,
+            height: 10
+        })
+        boxes.push({
+            x: 300,
+            y: height - 150,
+            width: 50,
+            height: 10
+        })
+        boxes.push({
+            x: 300,
+            y: 30,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 3) {
+        boxes.push({
+            x: 630,
+            y: height - 50,
+            width: 20,
+            height: 10
+        })
+        boxes.push({
+            x: 500,
+            y: height - 100,
+            width: 50,
+            height: 10
+        })
+        boxes.push({
+            x: 150,
+            y: height - 160,
+            width: 300,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: height - 45,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: height - 85,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: height - 125,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: height - 165,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 10,
+            y: height - 185,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 4) {
+        boxes.push({
+            x: 800,
+            y: height - 40,
+            width: 25,
+            height: 10
+        })
+        boxes.push({
+            x: 740,
+            y: height - 100,
+            width: 25,
+            height: 10
+        })
+        boxes.push({
+            x: 700,
+            y: height - 140,
+            width: 35,
+            height: 10
+        })
+        boxes.push({
+            x: 660,
+            y: height - 140,
+            width: 20,
+            height: 10
+        })
+        boxes.push({
+            x: 600,
+            y: height - 115,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 470,
+            y: height - 100,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 350,
+            y: height - 120,
+            width: 30,
+            height: 10
+        })
+        boxes.push({
+            x: 300,
+            y: height - 140,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 2) {
+        boxes.push({
+            x: 360,
+            y: 155,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 300,
+            y: 140,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 160,
+            y: 115,
+            width: 110,
+            height: 10
+        })
+        boxes.push({
+            x: 360,
+            y: 155,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 60,
+            y: 110,
+            width: 60,
+            height: 15
+        })
+        boxes.push({
+            x: 360,
+            y: 155,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 100,
+            y: 40,
+            width: 80,
+            height: 20
+        })
+        boxes.push({
+            x: 220,
+            y: 80,
+            width: 120,
+            height: 10
+        })
+        boxes.push({
+            x: 365,
+            y: 40,
+            width: 30,
+            height: 70
+        })
+        boxes.push({
+            x: 430,
+            y: 75,
+            width: 110,
+            height: 10
+        })
+        boxes.push({
+            x: 540,
+            y: 70,
+            width: 100,
+            height: 10
+        })
+        boxes.push({
+            x: 640,
+            y: 65,
+            width: 110,
+            height: 15
+        })
+        boxes.push({
+            x: 780,
+            y: 40,
+            width: 20,
+            height: 110
+        })
+        boxes.push({
+            x: 780,
+            y: 150,
+            width: 120,
+            height: 15
+        })
+        boxes.push({
+            x: 825,
+            y: 110,
+            width: 15,
+            height: 15
+        })
+
+    }
+    else if (level == 5) {
+        boxes.push({
+            x: 230,
+            y: 170,
+            width: 110,
+            height: 10
+        })
+        boxes.push({
+            x: 340,
+            y: 160,
+            width: 110,
+            height: 10
+        })
+        boxes.push({
+            x: 450,
+            y: 150,
+            width: 70,
+            height: 10
+        })
+        boxes.push({
+            x: 520,
+            y: 70,
+            width:20,
+            height: 80
+        })
+        boxes.push({
+            x: 590,
+            y: 145,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 590,
+            y: 115,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 540,
+            y: 70,
+            width: 90,
+            height: 10
+        })
+        boxes.push({
+            x: 420,
+            y: 70,
+            width: 70,
+            height: 10
+        })
+        boxes.push({
+            x: 285,
+            y: 35,
+            width: 85,
+            height: 10
+        })
+        boxes.push({
+            x: 400,
+            y: 20,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 460,
+            y: 20,
+            width: 30,
+            height: 10
+        })
+        boxes.push({
+            x: 520,
+            y: 25,
+            width: 140,
+            height: 10
+        })
+        boxes.push({
+            x: 640,
+            y: 10,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 6) {
+        boxes.push({
+            x: 30 ,
+            y: 160,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: 120,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 40,
+            y: 80,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 0,
+            y: 40,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 70,
+            y: 25,
+            width: 200,
+            height: 10
+        })
+        boxes.push({
+            x: 270,
+            y: 25,
+            width: 10,
+            height: 125
+        })
+        boxes.push({
+            x: 300,
+            y: 120,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 370,
+            y: 95,
+            width: 30,
+            height: 10
+        })
+        boxes.push({
+            x: 435,
+            y: 95,
+            width: 30,
+            height: 10
+        })
+        boxes.push({
+            x: 430,
+            y: 50,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 510,
+            y: 120,
+            width: 30,
+            height: 10
+        })
+        boxes.push({
+            x: 580,
+            y: 120,
+            width: 50,
+            height: 10
+        })
+        boxes.push({
+            x: 550,
+            y: 78,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 650,
+            y: 75,
+            width: 20,
+            height: 10
+        })
+        boxes.push({
+            x: 655,
+            y: 50,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 7) {
+        boxes.push({
+            x: 30,
+            y: 150,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 30,
+            y: 110,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 30,
+            y: 70,
+            width: 40,
+            height: 10
+        })
+        boxes.push({
+            x: 90,
+            y: 30,
+            width: 90,
+            height: 10
+        })
+        boxes.push({
+            x: 160,
+            y: 20,
+            width: 300,
+            height: 10
+        })
+        boxes.push({
+            x: 500,
+            y: 20,
+            width: 60,
+            height: 10
+        })
+        boxes.push({
+            x: 500,
+            y: 65,
+            width: 80,
+            height: 15
+        })
+        boxes.push({
+            x: 640,
+            y: 20,
+            width: 100,
+            height: 10
+        })
+        boxes.push({
+            x: 520,
+            y: 5,
+            width: 15,
+            height: 15
+        })
+    }
+    else if (level == 8) {
+        specials.push({
+            x: 5,
+            y: 192,
+            width: 20,
+            height: 8,
+            type: "lightButton"
+        })
+        boxes.push({
+            x: 250,
+            y: 150,
+            width: 80,
+            height:10
+        })
+        boxes.push({
+            x: 340,
+            y: 120,
+            width: 90,
+            height: 10
+        })
+        boxes.push({
+            x: 455,
+            y: 80,
+            width: 110,
+            height: 10
+        })
+        boxes.push({
+            x: 565,
+            y: 40,
+            width: 130,
+            height: 10
+        })
+        boxes.push({
+            x: 625,
+            y: 10,
+            width: 15,
+            height:15
+        })
+        platformLevelColor = "#1f1f1f"
+    }
+
+    return boxes;
+}
